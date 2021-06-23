b08c414d83310092939e531033211c5869160f1e
M Natoli <natoli.missy@gmail.com>
2021-06-23 18:04:31 -0500

    started api routes and got port to connect

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+router.get('/', (req, res) => {
+
+});
+
+
+
+module.exports = router;
