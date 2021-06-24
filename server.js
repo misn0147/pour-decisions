03a30dda18b467226c018622d85a37e7c3f10f39
Princess <princesstheaenriquez@gmail.com>
2021-06-23 19:28:30 -0500

    Added Handlebars template engine..

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const path = require('path');
 
 app.use(express.static(path.join(__dirname,'public')));
+
+const exhpbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine','handlebars');
