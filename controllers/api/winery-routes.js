2ae740c63746bfdf72264f4538e599af65f440ae
aw2389 <alex2@weghorst.com>
2021-06-23 19:04:48 -0500

    added Comment api route

--- controllers/api/winery-routes.js
+++ controllers/api/winery-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../../models');
+const { Winery, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
