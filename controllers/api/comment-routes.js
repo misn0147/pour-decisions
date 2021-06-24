b9c7bc898e2ce8093f398ed2ff74027d73bce01e
M Natoli <natoli.missy@gmail.com>
2021-06-23 20:28:57 -0500

    added home and dashboard routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -5,46 +5,46 @@ const { Comment } = require('../../models');
 
 //Get all comments by all users
 router.get('/', (req, res) => {
-  Comment.findAll()
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
 });
 
 //Created
 router.post('/', (req, res) => {
   // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id
-  })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+        });
 });
 
 //Delete Comment
 router.delete('/:id', (req, res) => {
-  Comment.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbCommentData => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: 'No comment found with this id!' });
-        return;
-      }
-      res.json(dbCommentData);
+    Comment.destroy({
+        where: {
+        id: req.params.id
+        }
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+        res.json(dbCommentData);
+        })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
 });
 module.exports = router;
